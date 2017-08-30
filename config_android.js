589018cd0a844c8884bffe558da9b32a5932a7d2
Martin Kohler <martin.kohler@hcu-hamburg.de>
2017-08-30 22:26:47 +0200

    Meister

--- config_android.js
+++ config_android.js
@@ -1,4 +1,4 @@
-exports.pics_incoming_folder = "~/storage/dcim/eyefi";
+exports.pics_incoming_folder = "~/storage/shared/Mobi";
 exports.pics_outgoing_folder = "./pics_outgoing/";
 exports.twitterMessage = "Big Urban Walks - Edition Sao Paulo 2017 #XPSP";
 exports.delayBeforePost = 10000; // milliseconds
