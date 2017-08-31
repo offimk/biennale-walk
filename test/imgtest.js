d72764d793e0189526ddecacce667bbb73aa9760
Martin Kohler <martin.kohler@hcu-hamburg.de>
2017-08-31 09:36:53 +0200

    Meister

--- test/imgtest.js
+++ test/imgtest.js
@@ -1,9 +1,18 @@
-var im = require("imagemagick");
-var file = "test.jpg";
-var fileout = "testout.jpg";
-
-  im.resize({width: 600, strip: false, srcPath: file, dstPath: fileout}, function(err) {
-      if(err) { throw err; }
-    //postImage(resentry);
-    //db.update({ _id: resentry._id }, { $set: { "status": 4} }, { multi: false });
-  });
+var fs = require('fs')
+  , gm = require('gm').subClass({imageMagick: true});
+ 
+// resize and remove EXIF profile data 
+gm('test.jpg')
+.resize(240, 240)
+.noProfile()
+.write('test_resized.jpg', function (err) {
+  if (err) throw(err);
+});
+ 
+ 
+// output all available image properties 
+gm('test.jpg')
+.identify(function (err, data) {
+  if (err) throw(err);
+});
+ 
