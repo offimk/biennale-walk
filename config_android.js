dd8057c7e663dc7da1a1739f713ec0af8c7e2a7e
offimk <parklands@gmx.net>
2019-07-30 18:34:20 +0200

    Delete config_android.js

--- config_android.js
+++
@@ -1,4 +0,0 @@
-exports.pics_incoming_folder = "~/storage/Download/movi/Mobi";
-exports.pics_outgoing_folder = "./pics_outgoing/";
-exports.twitterMessage = "Big Urban Walks - Bauforum Hamburg 2019 #BFHH";
-exports.delayBeforePost = 10000; // milliseconds
