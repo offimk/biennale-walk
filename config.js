e163b969a53b40fc507571e73f3a2d882f66a6d6
offimk <parklands@gmx.net>
2019-07-30 18:34:28 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-exports.pics_incoming_folder = "./pics_incoming/";
-exports.pics_outgoing_folder = "./pics_outgoing/";
-exports.twitterPost = "Test Status";
-exports.delayBeforePost = 3000; // milliseconds
